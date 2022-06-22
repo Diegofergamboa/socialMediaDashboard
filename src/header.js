8898d117e0f0f8705625fea3b5befb9395b845bc
Diego Fernando Ortiz Gamboa <diegofergamboa@hotmail.es>
2022-06-22 17:43:01 -0500

    CSS and modules imported

---
+++ src/header.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Header() {
+        return (
+        <header className='header'>
+            <div className='wrapper'>
+            <div className='header-grid'>
+                <div>
+                <h1>Social Media Dashboard</h1>
+                <p className='header-total'>Total followers: 23,004 </p>
+                </div>
+                <div className='dark-mode'>
+                <p className='dark-mode-title'>Dark Mode</p>
+                <input type='checkbox' className='checkbox' id='checkbox'/>
+                <label className='switch' htmlFor='checkbox'>
+    
+                </label>
+                </div>
+            </div>
+            </div>
+        </header>
+    );
+}
+
+export { Header } ;
+
