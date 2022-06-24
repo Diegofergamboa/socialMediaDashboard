af2ee23744bfd88ba16ae7f39e6e56d1563f307d
Diego Fernando Ortiz Gamboa <diegofergamboa@hotmail.es>
2022-06-23 19:06:15 -0500

    updated modules

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './globals.css';
 import { Header } from './header';
 import { TopCardList } from './top-card-list';
 import { Overview } from './overview';
+import { Switch } from './switch';
 
 function App() {
   return (
     <React.Fragment>
-      <Header/>
+      <Header>
+        <Switch/>
+      </Header>
       <TopCardList/>
       <Overview/>
     </React.Fragment>
