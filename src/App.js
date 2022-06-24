92310efd743e73875b68055f1edbb9135f7382e9
Diego Fernando Ortiz Gamboa <diegofergamboa@hotmail.es>
2022-06-23 19:39:03 -0500

    a

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import './globals.css';
 import { Header } from './header';
 import { TopCardList } from './top-card-list';
@@ -6,13 +6,38 @@ import { Overview } from './overview';
 import { Switch } from './switch';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false)
+  const [checked, setChecked] = useState(false)
+  const mainClass = darkMode ? 'is-dark-mode' : 'is-light-mode'
+
+  function changeMedia(mq) {
+      setDarkMode(mq.matches)
+      setChecked(mq.matches)
+  }
+
+  useEffect(() => {
+      const mq = window.matchMedia('(prefers-color-scheme: dark)')
+      mq.addListener(changeMedia)
+      setDarkMode(mq.matches)
+      setChecked(mq.matches)
+      return () => {
+          mq.removeListener(changeMedia)
+      }
+  }, [])
+
   return (
     <React.Fragment>
+      <main className={mainClass}>
       <Header>
-        <Switch/>
+        <Switch
+          setDarkMode={setDarkMode}
+          checked={checked}
+          setChecked={setChecked}
+        />
       </Header>
       <TopCardList/>
       <Overview/>
+      </main>
     </React.Fragment>
   );
 }
--- src/switch.js
+++ src/switch.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 import './switch.css';
 
 function Switch() {
-    return (
-        <div className='dark-mode'>
-            <p className='dark-mode-title'>Dark Mode</p>
-            <input type='checkbox' className='checkbox' id='checkbox'/>
-            <label className='switch' htmlFor='checkbox'>
+    const ref = useRef(null)
+    function handleChange() {
+        setChecked(ref.current.checked)
+        setDarkMode(ref.current.checked)
+    }
 
-            </label>
-        </div>
-    );
+    return (
+    <div className="dark-mode" >
+        <p className="dark-mode-title">Dark Mode</p>
+        <input ref={ref} onChange={handleChange} type="checkbox" checked={checked} className="checkbox" id="checkbox" />
+        <label className="switch" htmlFor="checkbox">
+
+        </label>
+    </div>
+    )
 };
 
 export { Switch } ;
